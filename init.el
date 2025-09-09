7ea4e7914be64776f55bacf2eb6146bbb59b16a9
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2025-09-09 17:40:44 -0600

    change theme to modus-vivendi-tinted

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
+ '(custom-enabled-themes '(modus-vivendi-tinted))
  '(package-selected-packages nil))
 
 
